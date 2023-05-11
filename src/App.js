519bc98bd74a0be509b3ec5517a74c2d958e4e0c
Fanuel Amare <101113291+Fanu-Cd@users.noreply.github.com>
2023-05-11 11:25:37 +0000

    corr16

--- src/App.js
+++ src/App.js
@@ -5,12 +5,15 @@ import {AiOutlineLeft} from 'react-icons/ai'
 import {AiOutlineRight} from 'react-icons/ai'
 import Button from '@mui/material/Button';
 import {BsFillCartFill} from 'react-icons/bs'
-
+import {SiOrigin} from 'react-icons/si'
+import {MdOutlineSentimentSatisfied} from 'react-icons/md'
+import {ImEnter} from 'react-icons/im'
+import {TbFreeRights} from 'react-icons/tb'
 const App=()=>{
   return(<>
        <Header />
        <section className="container front" style={{display:'flex',flexDirection:'column',borderRadius:'0.5rem',width:'100%',height:'30rem'}}>
-       <div style={{borderRadius:'0.5rem',marginTop:'1rem',marginLeft:'auto'}} className="btn-group btn-light" role="group" aria-label="Basic example">
+         <div style={{borderRadius:'0.5rem',marginTop:'1rem',marginLeft:'auto'}} className="btn-group btn-light" role="group" aria-label="Basic example">
           <button type="button" disabled className="btn btn-light"><AiOutlineLeft /></button>
           <button type="button" className="btn btn-light"><AiOutlineRight /></button>
         </div>
@@ -21,17 +24,18 @@ const App=()=>{
           <Button className="mt-5" size="medium" variant="contained" style={{color:'black',backgroundColor:'white'}} endIcon={<AiOutlineRight />}>Shop Now</Button>
         </div>
        </section>
-       <div className="container bg-info d-flex flex-column" style={{minHeight:"5rem",height:'auto'}}>
-          <h3>Brands</h3>
-          <div className="hstack bg-success" style={{minHeight:'3rem',height:'auto'}}>
-          </div>
-       </div>
        <div className="container px-2 d-flex flex-column" style={{minHeight:"5rem",height:'auto'}}>
-          <h3 className='textwrap' style={{maxWidth:'15rem'}}>We provide best customer experiences</h3>
+          <h3>Brands</h3>
+          <div className="container-fluid row" style={{minHeight:'3rem',height:'auto'}}>
+           </div> 
+          </div>
+          </div>
+       <div className="container px-2 d-flex flex-column" style={{minHeight:"5rem",height:'auto'}}>
+          <h3 className='textwrap' style={{maxWidth:'20rem'}}>We provide best customer experiences</h3>
           <div className="container-fluid row" style={{minHeight:'3rem',height:'auto'}}>
             <div className="card col-6 col-md-3 border-0">
               <div className="card-body">
-               <img style={{width:'30%',}} className='border rounded card-img-top' src={pic} alt="orig" />
+                <SiOrigin className='border rounded card-img-top' style={{width:'30%',minHeight:'2rem'}}/>
               <div style={{fontWeight:'bold'}} className="card-title">Original Products</div>
               <div className="card-text">
                 sfsfsfasfdfdsfsfsfsdfsdfsfsdfsdfsdf
@@ -40,7 +44,7 @@ const App=()=>{
             </div>
             <div className="card col-6 col-md-3 border-0">
               <div className="card-body">
-              <img style={{width:'30%',}} className='border rounded card-img-top' src={pic} alt="orig" />
+              <MdOutlineSentimentSatisfied className='border rounded card-img-top' style={{width:'30%',minHeight:'2rem'}}/>
               <div style={{fontWeight:'bold'}} className="card-title">Satisfaction Guarantee</div>
               <div className="card-text">
                 sfsfsfasfdfdsfsfsfsdfsdfsfsdfsdfsdf
@@ -48,21 +52,24 @@ const App=()=>{
             </div>
             </div>
             <div className="card col-6 col-md-3 border-0">
-            <img style={{width:'30%',}} className='border rounded card-img-top' src={pic} alt="orig" />
+             <div className="card-body">
+             <ImEnter className='border rounded card-img-top' style={{width:'30%',minHeight:'2rem'}}/>
               <div style={{fontWeight:'bold'}} className="card-title">New Arrival Everyday</div>
               <div className="card-text">
                 sfsfsfasfdfdsfsfsfsdfsdfsfsdfsdfsdf
               </div>
             </div>
+            </div>
             <div className="card col-6 col-md-3 border-0">
-            <img style={{width:'30%',}} className='border rounded card-img-top' src={pic} alt="orig" />
+             <div className="card-body">
+             <TbFreeRights className='border rounded card-img-top' style={{width:'30%',minHeight:'2rem'}}/>
               <div style={{fontWeight:'bold'}} className="card-title">Fast & Free Shopping</div>
               <div className="card-text">
                 sfsfsfasfdfdsfsfsfsdfsdfsfsdfsdfsdf
               </div>
             </div>
+            </div>
           </div>
-       </div>
        </>
   );
 }
