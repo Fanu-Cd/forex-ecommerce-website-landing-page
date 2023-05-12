57f4c33e8e7066e278569cba197a32035e64f84d
Fanuel Amare <93039064+Fanu-el@users.noreply.github.com>
2023-05-12 13:17:01 +0000

    corr20

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {AiOutlineLeft} from 'react-icons/ai'
 import {AiOutlineRight} from 'react-icons/ai'
 import Button from '@mui/material/Button';
 import {BsFillCartFill} from 'react-icons/bs'
+import {BsArrowRight} from 'react-icons/bs'
 import {SiOrigin} from 'react-icons/si'
 import {MdOutlineSentimentSatisfied} from 'react-icons/md'
 import {ImEnter} from 'react-icons/im'
@@ -26,27 +27,27 @@ const App=()=>{
        <Header />
        <div id="carouselExample" className="rounded container front carousel slide" data-bs-ride="carousel">
          <div className="btn-group rounded z-index-2 d-block position-absolute" style={{top:'1rem',left:'3rem',zIndex:'2',backgroundColor:'white'}}>
-  <button className="btn" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-    <AiOutlineLeft />
-  </button>
-  <button className="btn" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-    <AiOutlineRight/>
-  </button>
-         </div>
-         <div className="carousel-inner rounded">
-    <div className="carousel-item active">
-      <img src={p1} className="d-block w-100 " alt="..." style={{objectFit:'cover',height:'25rem'}} />
-    </div>
-    <div className="carousel-item">
-      <img src={p2} className="d-block w-100" alt="..." style={{objectFit:'cover',height:'25rem'}}  />
-    </div>
-    <div className="carousel-item">
-      <img src={p3} className="d-block w-100" alt="..." style={{objectFit:'cover',height:'25rem'}}  />
-    </div>
-         </div>
+            <button className="btn" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+              <AiOutlineLeft />
+            </button>
+            <button className="btn" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+              <AiOutlineRight/>
+            </button>
         </div>
+         <div className="carousel-inner rounded">
+          <div className="carousel-item active">
+           <img src={p1} className="d-block w-100 " alt="..." style={{objectFit:'cover',height:'25rem'}} />
+          </div>
+          <div className="carousel-item">
+           <img src={p2} className="d-block w-100" alt="..." style={{objectFit:'cover',height:'25rem'}}  />
+          </div>
+          <div className="carousel-item">
+           <img src={p3} className="d-block w-100" alt="..." style={{objectFit:'cover',height:'25rem'}}  />
+          </div>
+          </div>
+         </div>
         <div className='d-flex justify-content-center'>
-          <Button variant='contained' className='btn text-body position-relative bg-white' style={{bottom:'6rem'}} endIcon={<AiOutlineRight />}>Shop Now</Button>
+          <Button variant='contained' className='btn text-body position-relative bg-white' style={{bottom:'6rem'}} endIcon={<BsArrowRight />}>Shop Now</Button>
         </div>
        <div className="container mt-3 px-2 d-flex flex-column" style={{height:"5rem",height:'auto'}}>
           <h3 className="text-center text-lg-left" style={{fontWeight:'bold'}}>Brands</h3>
@@ -102,7 +103,7 @@ const App=()=>{
             </div>
           </div>
         </div>
-        <div className="container row mx-auto">
+      <div className="container row mx-auto">
             <div style={{height:'15rem'}} className="rounded col-sm-6 col-12">
               <img src={p1} className='rounded' style={{width:'100%',objectFit:'cover',height:'100%'}} alt='Best Seller' />
             </div>
@@ -115,6 +116,20 @@ const App=()=>{
             <div style={{height:'15rem'}} className=" rounded col-sm-6 col-12">
               <img src={p4} className='rounded' style={{width:'100%',objectFit:'cover',height:'100%'}} alt='Best Seller' />
             </div>
+        </div>
+        <div className="container row mx-auto my-3 rounded" style={{minHeight:'15rem'}}>
+          <div className='col-4'>
+            <img className='rounded-left' style={{objectFit:'cover',width:'100%',height:'15rem'}} src={p2} />
+          </div>
+          <div style={{height:'15rem'}} className='rounded-right col-8 bg-dark d-flex flex-column justify-content-center align-items-center'>
+              <h6 className=" text-white text-center py-2">LIMITED OFFER</h6>
+              <p style={{fontSize:'1.5rem',fontWeight:'bold'}} className="text-white py-2 text-center">35% off only this friday and get special gift</p>
+             <Button size='small' variant='contained' className='btn text-body bg-white' endIcon={<BsArrowRight />}>Grab it now</Button>
+          </div>
+        </div>
+        <div className='mx-auto d-flex flex-column justify-content-center align-items-center bg-info' style={{height:'auto',width:'70%'}}>
+            <h3 style={{fontWeight:'bold'}} className='h3 text-center'>Subscribe to our newsletter to get updates to our latest collections</h3>
+            <p></p>
         </div>
        </>
   );
